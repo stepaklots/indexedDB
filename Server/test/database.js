569724782396e60256491061313460ed651b2410
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-06-30 13:47:04 +0300

    Add github actions CI

---
+++ Server/test/database.js
@@ -0,0 +1,13 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import 'fake-indexeddb/auto';
+import { Database } from '../static/storage.js';
+
+test('Database connects and exposes repository', async () => {
+  const entities = [
+    { name: 'user', options: { keyPath: 'id', autoIncrement: true } }
+  ];
+  const db = await new Database('TestDatabase', 1, entities);
+  const repo = db.getRepo('user');
+  assert.ok(repo);
+});
