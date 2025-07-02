b32ba471f967ae69363803341cff758e391a22d2
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-07-02 15:49:56 +0300

    Add more tests

--- Enterprise/test/user.js
+++ Enterprise/test/user.js
@@ -15,7 +15,36 @@ test('Enterprise: UserModel validation', async () => {
   assert.strictEqual(user.age, 42);
 });
 
-test('Enterprise: UserService, UserRepository', async () => {
+test('Enterprise: UserRepository', async () => {
+  const db = new Database('UserRepositoryTestDB', 1, (db) => {
+    if (!db.objectStoreNames.contains('user')) {
+      db.createObjectStore('user', { keyPath: 'id', autoIncrement: true });
+    }
+  });
+  await db.connect();
+
+  const userRepo = new UserRepository(db, 'user');
+
+  const user1 = new UserModel('Lucius', 17);
+  await userRepo.insert(user1);
+  const user2 = new UserModel('Antoninus', 33);
+  await userRepo.insert(user2);
+  const user3 = new UserModel('Faustina', 18);
+  await userRepo.insert(user3);
+
+  const user = await userRepo.get(1);
+  assert.equal(user.name, 'Lucius');
+
+  user.age += 1;
+  await userRepo.update(user);
+  assert.equal(user.age, 18);
+
+  const users = await userRepo.getAll();
+  assert.equal(users.length, 3);
+  assert.equal(users[1].age, 33);
+});
+
+test('Enterprise: UserService', async () => {
   const db = new Database('ServiceTestDB', 1, (db) => {
     if (!db.objectStoreNames.contains('user')) {
       db.createObjectStore('user', { keyPath: 'id', autoIncrement: true });
